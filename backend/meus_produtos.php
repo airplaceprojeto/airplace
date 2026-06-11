20b5f2218df715cc2ffb7e6b576e6c2dac9e6b66
Leonardo De Moura Souza <109994680+leonardodemoura@users.noreply.github.com>
2026-06-11 14:46:19 -0300

    Update meus_produtos.php
    adicionando a aba atendetimentos ao nav

--- backend/meus_produtos.php
+++ backend/meus_produtos.php
@@ -164,6 +164,7 @@ try {
             <li><a href="produtos.php">Produtos</a></li>
             <li><a href="drones.php">Conheça nossos Drones</a></li>
             <li><a href="airplay.php">Air Play</a></li>
+            <li><a href="atendimentos.php">Atendimento IA</a></li>
         </ul>
     </nav>
 </header>
