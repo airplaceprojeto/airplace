ba73cd633878a3bd9aa3615fdc16c221434c40ce
Leonardo De Moura Souza <109994680+leonardodemoura@users.noreply.github.com>
2026-06-11 14:48:12 -0300

    Update sobrenos.php
    adicionando a aba atendimentos  ao nav

--- backend/sobrenos.php
+++ backend/sobrenos.php
@@ -394,6 +394,7 @@ session_start(); // Inicia a sessão
             <li><a href="produtos.php">Produtos</a></li>
             <li><a href="drones.php">Conheça nossos Drones</a></li>
             <li><a href="airplay.php">Air Play</a></li>
+            <li><a href="atendimentos.php">Atendimento IA</a></li>
         </ul>
     </nav>
 </header>
