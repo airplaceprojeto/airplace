d88fabc51e495484edaf9f3257bbcf2554fe8c4b
Leonardo De Moura Souza <109994680+leonardodemoura@users.noreply.github.com>
2026-06-11 14:45:08 -0300

    Update editar_produto.php
    adicionando a aba atendimentos ao nav

--- backend/editar_produto.php
+++ backend/editar_produto.php
@@ -75,6 +75,7 @@ if (isset($_GET['id'])) {
                        <li><a href="cupons.php">Cupons</a></li>
                        <li><a href="drones.php">Conheça nossos Drones</a></li>
                        <li><a href="airplay.php">Air Play</a></li>
+                       <li><a href="atendimentos.php">Atendimento IA</a></li>
                 </ul>
             </nav>
 
